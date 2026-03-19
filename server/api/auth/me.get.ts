4803bb0b3dcba5d6540c40b39417741fa9148287
dj1n19 <bruno.genie@hotmail.com>
2026-03-19 16:51:16 +0100

    add api route me.gets.ts

---
+++ server/api/auth/me.get.ts
@@ -0,0 +1,26 @@
+import { db } from '../../db/index'
+import { users } from '../../db/schema'
+import { eq } from 'drizzle-orm'
+
+export default defineEventHandler(async (event) => {
+    const session = await useSession(event, {
+        password: process.env.SESSION_SECRET!
+    })
+
+    if (!session.data.userId) {
+        throw createError({ statusCode: 401, message: "Not authenticated" })
+    }
+
+    const user = await db.select().from(users).where(eq(users.id, session.data.id)).get()
+
+    if (!user) {
+        throw createError({ statusCode: 401, message: "User not found" })
+    }
+
+    return {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        role: user.role
+    }
+})
