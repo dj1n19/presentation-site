9a0267a834b94403bf1be957e5146f1d59c6c1a1
dj1n19 <bruno.genie@hotmail.com>
2026-03-20 19:18:20 +0100

    add logout button component

---
+++ app/components/ui/LogoutButton.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+	const router = useRouter()
+	const toast = useToast()
+
+	async function logout() {
+		try {
+			await $fetch('/api/auth/logout', {
+				method: 'POST'
+			})
+			await router.push('/');
+		}
+		catch (e: any) {
+			toast.add({
+				title: 'Erreur',
+				description: e?.data?.message ?? 'Logout failed',
+				color: 'error'
+			})
+		}
+	}
+</script>
+
+<template>
+    <UButton @click="logout">Logout</UButton>
+</template>
